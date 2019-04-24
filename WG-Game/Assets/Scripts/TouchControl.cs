b86faa63c0da1b4cc09839a0a1308953fb1cfcc8
Christoph <christophweinreich@googlemail.com>
2019-04-24 15:12:22 +0200

    Scheiß auf Würfel

--- WG-Game/Assets/Scripts/TouchControl.cs
+++ WG-Game/Assets/Scripts/TouchControl.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class TouchControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+        }
     }
 }
